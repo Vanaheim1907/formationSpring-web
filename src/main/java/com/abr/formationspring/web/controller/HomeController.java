24fa3ce0154cb8c7b7500137957deafa5f372aa8
Alexandre <alexandrebrosteau@gmail.com>
2023-08-23 06:50:57 +0200

    fix pom

--- src/main/java/com/abr/formationspring/web/controller/HomeController.java
+++ src/main/java/com/abr/formationspring/web/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.abr.formationspring.web.controller;
 import com.abr.formationspring.core.entity.Movie;
 import com.abr.formationspring.core.service.MovieServiceInterface;
 import com.abr.formationspring.web.form.MovieForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
     private final MovieServiceInterface service;
 
     public HomeController(MovieServiceInterface service) {
